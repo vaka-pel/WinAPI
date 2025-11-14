c8fb60695d8d68d191a37dc2f6d8940698dd5427
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-11-14 21:55:54 +0300

    Copy DONE; TODO:Placeholder.

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -26,10 +26,25 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG: // выполн€етс€ один раз при запуске окна.
+	{
+		HICON hicon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hicon);
+		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
+	}
 		break;
 	case WM_COMMAND: //обрабатывает команды с клавиатуры и мыши.
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+			break;
 		case IDOK:
 			MessageBox(NULL, "Ѕыла нажата кнопка 'OK'", "info", MB_OK | MB_ICONINFORMATION);
 			break;
--- WinAPI/resource.h
+++ WinAPI/resource.h
@@ -3,20 +3,21 @@
 // Used by Resource.rc
 //
 #define IDD_DIALOG1                     101
+#define IDI_ICON1                       103
 #define IDC_STATIC_LOGIN                1001
 #define IDC_CHECK1                      1002
 #define IDC_EDIT_LOGIN                  1005
 #define IDC_EDIT_PASSWORD               1006
-#define IDC_BUTTON1                     1007
 #define IDC_BUTTON_COPY                 1007
+#define IDC_EDIT1                       1009
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
