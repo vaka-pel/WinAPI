60090ae336fbf7a8cc3afdf6185970db937e31a2
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-11-24 21:28:10 +0300

    Edit elements of ListBox Done

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -1,16 +1,15 @@
 #include<Windows.h>
 #include"resource.h"
 
-CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "List", "Box" };
+CONST CHAR * g_sz_VALUES[] = { "This", "is", "my", "first", "List", "Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcEdit(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	//hinstance - экзэмпляр исполняемого файла программы, загруженного в память (*.exe)
+	//hInstance - это экземпляр исполняемого файла программы, загруженного в память (*.exe)
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, (DLGPROC)DlgProc, 0);
 	return 0;
 }
@@ -32,14 +31,14 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 		case IDC_LIST1:
 			if (HIWORD(wParam) == LBN_DBLCLK)
-				//GetModuleHandle(NULL) - Возвращает "hinstance" нашей программе
-				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, (DLGPROC) DlgProcEdit, 0);
+				//GetModuleHandle(NULL);	//Возвращает 'hInstance' нашей программы.
+				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, (DLGPROC)DlgProcEdit, 0);
 			break;
 		case IDC_BUTTON_ADD:
 		{
-			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, (DLGPROC) DlgProcAdd, 0);
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd,(DLGPROC) DlgProcAdd, 0);
 		}
-    	break;
+		break;
 		case IDC_BUTTON_DELETE:
 		{
 			HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
@@ -113,11 +112,11 @@ BOOL CALLBACK DlgProcEdit(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HWND hList = GetDlgItem(hParent, IDC_LIST1);
 		INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
 		SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
-		SendMessage(hList, WM_SETTEXT, i, (LPARAM)sz_buffer);
+		SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		SetFocus(hEdit);
-		SendMessage(hEdit, EM_SETSEL, 256, -1);
+		SendMessage(hEdit, EM_SETSEL, strlen(sz_buffer), -1);
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
@@ -127,22 +126,20 @@ BOOL CALLBACK DlgProcEdit(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 
-		    HWND hParent = GetParent(hwnd);
-		    HWND hList = GetDlgItem(hParent, IDC_LIST1);
-		    INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			HWND hParent = GetParent(hwnd);
+			HWND hList = GetDlgItem(hParent, IDC_LIST1);
+			INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
 			if (SendMessage(hList, LB_FINDSTRINGEXACT, 0, (LPARAM)sz_buffer) == LB_ERR)
 			{
-		        SendMessage(hList, LB_DELETESTRING, i, NULL);
-				SendMessage(hEdit, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
-
+				SendMessage(hList, LB_DELETESTRING, i, NULL);
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
 			}
-			
 		}
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 		}
 	}
-		break;
+	break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 	}
