b5c398dafdafa6587ab7a25d875a4721afbe2307
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-11-26 22:18:35 +0300

    ToDo: MessageLoop

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,8 +1,9 @@
 #include<Windows.h>
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My first window";
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-INT WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	WNDCLASSEX wClass;
 	ZeroMemory(&wClass, sizeof(wClass));
@@ -15,6 +16,62 @@ INT WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 	//инициализируем внешний вид окон:
 	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+	// инициализация системных переменных:
+	wClass.hInstance = hInstance;
+	wClass.lpszClassName = g_sz_WINDOW_CLASS;
+	wClass.lpszMenuName = NULL;
+	wClass.lpfnWndProc = WndProc;
+
+	if (RegisterClassEx(&wClass) == NULL)
+	{
+		MessageBox(NULL, "Class registration failed", NULL, MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,
+		g_sz_WINDOW_CLASS,    //Имя класса окна
+		g_sz_WINDOW_CLASS,     //Заголовок окна
+		WS_OVERLAPPEDWINDOW,           // стиль окна, стили зависят от класса окна
+		CW_USEDEFAULT, CW_USEDEFAULT,    //position
+		CW_USEDEFAULT, CW_USEDEFAULT,    // размер окна
+		NULL,    //для главного окна это ResourseID главного меню
+		NULL,    //
+		hInstance,
+		NULL
+	);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Windows creation failed", NULL, MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
 
 	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return FALSE;
 }
